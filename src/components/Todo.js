71214e678f977f75b3d9d378d3fd2f95e0723ebc
nebojsadj <djordjevicn87@gmail.com>
2020-10-06 13:24:29 +0200

    useDispatch

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
-import store from "../redux/store";
 import {
   load_action,
   mark_action,
   delete_action,
 } from "../redux/actionCreators";
+import { useDispatch } from "react-redux";
 
 function Todo({ todo }) {
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    store.dispatch(load_action());
+    dispatch(load_action());
   }, []);
 
   return (
@@ -17,14 +19,14 @@ function Todo({ todo }) {
         <h2 className={todo.done ? "done" : "notDone"}>{todo.task}</h2>
         <button
           onClick={() => {
-            store.dispatch(mark_action(todo.id));
+            dispatch(mark_action(todo.id));
           }}
           className="btn btn-warning btn-sm float-left"
         >
           To mark
         </button>
         <button
-          onClick={() => store.dispatch(delete_action(todo.id))}
+          onClick={() => dispatch(delete_action(todo.id))}
           className="btn btn-danger btn-sm float-right"
         >
           Delete
