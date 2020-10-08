28b77c06749d53e3df9c5ddb6fa9a579116b8b97
nebojsadj <djordjevicn87@gmail.com>
2020-10-08 23:41:09 +0200

    removed allTodo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,17 +5,17 @@ import { useSelector } from "react-redux";
 function TodoList() {
   const todoList = useSelector((state) => state.todoList);
 
-  let allList = todoList.map((todo, index) => (
-    <div className="col-6 offset-3 mt-4" key={index}>
-      <Todo todo={todo} />
-    </div>
-  ));
-
   return (
     <div className="container mt-4">
       <div className="row">
         <div className="col-8 offset-2">
-          <div className="row">{allList}</div>
+          <div className="row">
+            {todoList.map((todo, index) => (
+              <div className="col-6 offset-3 mt-4" key={index}>
+                <Todo todo={todo} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
